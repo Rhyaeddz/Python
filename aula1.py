eaab288aa9c6c0d354c1e7bb35e2c82a6e398343
Rhyaeddz <Rafael.francisco.silvagld@gmail.com>
2024-09-04 07:30:16 -0300

    Comitei B)

--- aula1.py
+++ aula1.py
@@ -1,1 +1,16 @@
 print('hello World')
+
+ #git add. "nome da pasta" disponibiliza pasta para ser comitada,
+ #git commit -m - comita e git push manda do vscode pro github ,
+ #git status, 
+ #git checkout -b (cria uma branch/ramificação),
+ #git pull,
+
+# "#" serve para comentarios e não são lidos pelo sistema, servem apenas para a linha que está a #,
+
+"""
+"tres aspas" chamado de DocStrings
+serve de comentario e são lidas pelo sistema ;)
+"""
+ 
+ 
---
+++ aula3.py
@@ -0,0 +1,10 @@
+"""
+str -> string -> texto
+strings são textos dentro de aspas 
+
+"""
+#aspas simples
+print('neymar "junior"',69,7, sep=" "),
+print("neymar 'junior'"),
+print("\"menino\" ney"),
+print(r"errei \'fui mlk\'")
